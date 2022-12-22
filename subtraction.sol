92c44f53b1196ab767e7c4fab87b1fc4e7051b0c
Maithali <63653718+ghardemaithaly@users.noreply.github.com>
2022-12-22 13:45:10 +0530

    Create subtraction.sol

---
+++ subtraction.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.5;
+
+contract FunctionsIntro {
+// subtraction of two number
+    function substraction(int a, int b) external pure returns(int){
+        return a-b;
+    }
+}
