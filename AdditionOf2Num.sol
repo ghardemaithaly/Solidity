347afd337a75b3c2f5e52b22c9a24d37d94f18bd
Maithali <63653718+ghardemaithaly@users.noreply.github.com>
2022-12-22 13:40:07 +0530

    Create AdditionOf2Num.sol

---
+++ AdditionOf2Num.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.5;
+
+contract FunctionsIntro {
+    //add two numbers
+    function addition(uint x, uint y) external pure returns (uint ){
+     return x+y;
+    }
+}
