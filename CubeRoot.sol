4c8b9f8cc40a9b6b47d3a0a21af3dbd6385c6d25
Maithali <63653718+ghardemaithaly@users.noreply.github.com>
2022-12-23 14:23:41 +0530

    Update CubeRoot.sol

--- CubeRoot.sol
+++ CubeRoot.sol
@@ -1,12 +1,9 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.3;
 
-contract Check{
-
-    function EvenOdd(uint n) pure public returns(uint){
-        if(n %2==0){
-            return 1;
-        }else
-        return 0;
+contract CubeRoot{
+  
+    function CubeRootIs(uint cube) pure  public  returns(uint){
+        return cube * cube * cube;
     }
 }
